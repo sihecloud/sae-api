c5bb3de685d9f03269517a0b1fa4d7c005d46d4a
dongzhou.xie <dongzhou.xie@qq.com>
2023-05-12 12:35:12 +0800

    update component metadata, and add command entries

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,35 @@ import { InputProps } from './common/entity';
 
 export default class ComponentDemo {
   /**
-   * demo 实例
+   * 列出应用列表
    * @param inputs
    * @returns
    */
-  public async test(inputs: InputProps) {
+  public async list(inputs: InputProps) {
     logger.debug(`input: ${JSON.stringify(inputs.props)}`);
-    logger.info('command test');
+    logger.info('list test');
+    return { hello: 'world' };
+  }
+
+  /**
+   * 版本更新
+   * @param inputs
+   * @returns
+   */
+  public async upgrade(inputs: InputProps) {
+    logger.debug(`input: ${JSON.stringify(inputs.props)}`);
+    logger.info('upgrade test');
+    return { hello: 'world' };
+  }
+
+  /**
+   * 扩容缩容
+   * @param inputs
+   * @returns
+   */
+  public async scale(inputs: InputProps) {
+    logger.debug(`input: ${JSON.stringify(inputs.props)}`);
+    logger.info('scale test');
     return { hello: 'world' };
   }
 }
